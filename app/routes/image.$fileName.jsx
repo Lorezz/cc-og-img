faf93d5999aefca20059f9f4f3825061a7417f40
Lorezz <point.point@gmail.com>
2022-04-21 23:56:38 +0200

    use puppeteer core, passing qs wip

--- app/routes/image.$fileName.jsx
+++ app/routes/image.$fileName.jsx
@@ -1,5 +1,14 @@
-import puppeteer from 'puppeteer';
-export async function loader({ params }) {
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
+const exePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+export async function loader({ request, params }) {
+  const url = new URL(request.url);
+  let qs = {};
+  for (const [key, value] of url.searchParams.entries()) {
+    qs[key] = value;
+  }
+  const { preview } = qs;
+  console.log('qs', qs);
   console.log('params', params);
   const { fileName } = params;
   const [name, type] = fileName.split('.');
@@ -16,7 +25,7 @@ export async function loader({ params }) {
         padding:0;
         width:100vw;
         height:100vh;
-        background: #222 ;
+        background:${qs?.bg || '#222'};
         color:#fefefe;
         display:flex;
         align-items:center;
@@ -26,9 +35,34 @@ export async function loader({ params }) {
     <h1>HELLO WORLD</h1>
   </body>
   </html>`;
-  const browser = await puppeteer.launch({ headless: true });
+
+  let options;
+  let viewport = preview
+    ? { width: 612, height: 252, deviceScaleFactor: 1 }
+    : { width: 2048, height: 1170, deviceScaleFactor: 1 };
+  const isDev = process.env.NODE_ENV
+    ? process.env.NODE_ENV === 'development'
+    : true;
+  console.log('isDev', isDev);
+  if (isDev) {
+    //viewport = { ...viewport, deviceScaleFactor: 0.5 };
+    // viewport = { width: 612, height: 252, deviceScaleFactor: 1 };
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+    };
+  } else {
+    options = {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    };
+  }
+
+  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
-  page.setViewport({ width: 800, height: 600, deviceScaleFactor: 1 });
+  page.setViewport(viewport);
   await page.setContent(content);
   const pic = await page.screenshot({
     // path: `/tmp/${name}-${Date.now()}.${type}`,
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,5 @@
 import { json } from '@remix-run/node';
-import { useActionData, Form, useTransition } from '@remix-run/react';
+import { useActionData, Form, useTransition, Link } from '@remix-run/react';
 
 import unsplashSearch from '~/lib/unsplash.js';
 import brandSearch from '~/lib/brandfetch.js';
@@ -22,6 +22,8 @@ export async function action({ request }) {
 export default function Index() {
   const transition = useTransition();
   const actionData = useActionData();
+  const picUrl = `/image/pic.png?bg=pink`;
+  const previewUrl = `/image/pic.png?bg=pink&preview=true`;
   console.log('actionData', actionData);
   return (
     <div
@@ -34,10 +36,27 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <div
         style={{
-          display: 'flex',
+          // display: 'flex',
           background: '#fefefe',
         }}
       >
+        <div>
+          <div
+            style={{
+              background: 'black',
+              width: 612,
+              height: 252,
+              overflow: 'hidden',
+              backgroundImage: `url(${previewUrl})`,
+              backgroundSize: 'object-fit',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+            }}
+          />
+          <a href={picUrl} target="preview">
+            final image
+          </a>
+        </div>
         <div>
           <Form method="post">
             <label>SEARCH UNSPLASH</label>
@@ -74,9 +93,6 @@ export default function Index() {
                 </p>
               </div>
             ))}
-        </div>
-        <div>
-          <img src="/image/pic.png" alt="pic" />
         </div>
       </div>
     </div>
